62316b3f9056da724c44e88867d59238954b6ccb
Zizu02 <habibyazar7@gmail.com>
2024-09-07 16:36:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Güvenli bir anahtar belirleyin
-CORS(app)
+CORS(app, supports_credentials=True)
 
 # Veritabanı simülasyonu
 users = []
