4ac5dce9db8c0f07b2af4e0021f7d365851be00c
Zizu02 <habibyazar7@gmail.com>
2024-09-07 15:50:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Veritabanı simülasyonu
 users = []
@@ -29,3 +31,4 @@ def create_account():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
