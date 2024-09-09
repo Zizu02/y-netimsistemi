0cbeda49db38c67d0b33edeafe508a1fcedb97dd
Zizu02 <habibyazar7@gmail.com>
2024-09-09 16:04:51 +0300

    Update models.py

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ migrate = Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)  # password yerine password_hash
     address = db.Column(db.String(255), nullable=True)
     phone = db.Column(db.String(20), nullable=True)
 
