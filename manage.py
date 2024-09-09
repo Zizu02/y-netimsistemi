a8f5f27fb6378d46ba5d1a37277c71cf14680246
Zizu02 <habibyazar7@gmail.com>
2024-09-09 16:06:36 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_cors import CORS
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://depo_user:password@dpg-cretkstsvqrc73fmrhp0-a.frankfurt-postgres.render.com/depo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://depo_user:your_password@dpg-cretkstsvqrc73fmrhp0-a.frankfurt-postgres.render.com/depo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-CORS(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)  # password yerine password_hash
     address = db.Column(db.String(255), nullable=True)
     phone = db.Column(db.String(20), nullable=True)
 
@@ -31,5 +31,10 @@ class User(db.Model):
             'phone': self.phone
         }
 
+# Flask-Script Manager'ı başlatın
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    manager.run()
+
