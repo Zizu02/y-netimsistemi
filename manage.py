a24af0f006463299e0daadc14d7e6625bef619bb
Zizu02 <habibyazar7@gmail.com>
2024-09-09 15:14:45 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://depo_user:fyL02LkCj6DJnyf2oE7rLTvgGa2mSVOC@dpg-cretkstsvqrc73fmrhp0-a.frankfurt-postgres.render.com/depo'
@@ -14,22 +15,26 @@ CORS(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)  # Şifreyi saklamak için
-    address = db.Column(db.String(255), nullable=True)  # Adres
-    phone = db.Column(db.String(20), nullable=True)  # Telefon
+    password_hash = db.Column(db.String(128), nullable=False)
+    address = db.Column(db.String(255), nullable=True)
+    phone = db.Column(db.String(20), nullable=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def to_dict(self):
         return {
             'id': self.id,
             'email': self.email,
-            'password': self.password,
             'address': self.address,
             'phone': self.phone
         }
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
