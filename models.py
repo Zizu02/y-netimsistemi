877ada7230ca7c4f5a9daeba32699c587d80a9f5
Zizu02 <habibyazar7@gmail.com>
2024-09-09 11:13:45 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=True)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'name': self.name
+        }
