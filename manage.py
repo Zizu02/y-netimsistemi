1672d8db09da6c5172731359d7bbb03a072daa0c
Zizu02 <habibyazar7@gmail.com>
2024-09-09 13:50:02 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -21,5 +21,5 @@ class User(db.Model):
             'email': self.email
         }
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
