4841086df19c8e82bbdfee640b2e45e314018d29
Zizu02 <habibyazar7@gmail.com>
2024-09-09 12:54:23 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,30 @@
-from app import app, db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://depo_user:fyL02LkCj6DJnyf2oE7rLTvgGa2mSVOC@dpg-cretkstsvqrc73fmrhp0-a.frankfurt-postgres.render.com/depo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'email': self.email
+        }
+
+@manager.command
+def run():
+    app.run()
 
 if __name__ == "__main__":
     manager.run()
