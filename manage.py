e39ac7ff1bc853239d6ebcf027b8896a517d9cde
Zizu02 <habibyazar7@gmail.com>
2024-09-09 15:43:31 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,9 +5,7 @@ from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://depo_user:fyL02LkCj6DJnyf2oE7rLTvgGa2mSVOC@dpg-cretkstsvqrc73fmrhp0-a.frankfurt-postgres.render.com/depo'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://depo_user:password@dpg-cretkstsvqrc73fmrhp0-a.frankfurt-postgres.render.com/depo'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 CORS(app)
@@ -18,9 +16,6 @@ class User(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     address = db.Column(db.String(255), nullable=True)
     phone = db.Column(db.String(20), nullable=True)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
